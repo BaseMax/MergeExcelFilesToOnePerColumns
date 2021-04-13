538a220982e134d87dd4ad5ada1519fbcd8209c4
Max Base <MaxBaseCode@Gmail.Com>
2021-04-13 23:51:40 +0430

    Solve problem and add features

--- parser.php
+++ parser.php
@@ -4,16 +4,23 @@
 // 2021-04-13
 
 require "excel.php";
+require "excel-create.php";
 
 $authors = [];
 $counts = [];
+$data = [];
 
 $files = glob("*.xlsx");
 $files_count = 0;
-foreach($files as $file) {
+// foreach($files as $file) {
+for($i=1;$i<=65;$i++) {
+	$file = $i.".xlsx";
 	if($file === "" || $file === "." || $file === "..") {
 		continue;
 	}
+	// if($files_count > 5) {
+	// 	// break;
+	// }
 	$files_count++;
 	// print $file."\n";
 	if($xlsx = SimpleXLSX::parse($file) ) {
@@ -21,14 +28,29 @@ foreach($files as $file) {
 		// print_r($rows);
 		$values = [];
 		foreach($rows as $row) {
-			if(isset($row[0], $row[1]) && $row[0] !== "" && $row[1] !== "") {
-				$row[1] = $row[1];
+			if(isset($row[1])) {
 				$values[] = $row[1];
+			}
+			else {
+				$values[] = "";
 			}
 		}
 		// print_r($values);
-		$counts[] = (int) count($rows);
+		$counts[] = (int) count($values);
 		$authors[] = $values;
+		// exit();
+		// $values = [];
+		// foreach($rows as $row) {
+		// 	print_r($row);
+		// 	if(isset($row[0], $row[1]) && $row[0] !== "" && $row[1] !== "") {
+		// 		$row[1] = $row[1];
+		// 		$values[] = $row[1];
+		// 	}
+		// }
+		// // print_r($values);
+		// $counts[] = (int) count($values);
+		// $authors[] = $values;
+		// exit();
 	} else {
 		echo SimpleXLSX::parseError();
 	}
@@ -36,8 +58,27 @@ foreach($files as $file) {
 
 // print_r($authors);
 
-// $m = max($counts);
-// $c = count($authors);
+$m = max($counts);
+// print $m."\n";
+// exit();
+$c = count($authors);
+// print count($authors[0])."\n";
+// exit();
+
+for($i=1;$i<=$m;$i++) {
+	$data[$i-1] = [];
+}
+
+for($i=1;$i<=$m;$i++) {
+	for($j=1;$j<=$c;$j++) {
+		$data[$i-1][$j-1] = "";
+		if(isset($authors[$j-1][$i-1])) {
+			$data[$i-1][$j-1] = $authors[$j-1][$i-1];
+		}
+	}
+}
+// print_r($data);
+// exit();
 
 // for($i=1;$i<=$c;$i++) {
 // 	for($j=1;$j<=$m;$j++) {
@@ -49,19 +90,26 @@ foreach($files as $file) {
 // 	print "\n";
 // }
 
-for($j=0;$j<500;$j++) {
-	$has = false;
-	for($i=0;$i<$files_count;$i++) {
-		if(isset($authors[$i][$j])) {
-			$authors[$i][$j] = trim($authors[$i][$j]);
-			if($authors[$i][$j] !== "") {
-				$has = true;
-				print $authors[$i][$j];
-				print "\t";
-			}
-		}
-	}
-	if($has === true) {
-		print "\n";
-	}
-}
+// exit();
+
+// for($j=0;$j<500;$j++) {
+// 	$has = false;
+// 	for($i=0;$i<$files_count;$i++) {
+// 		print $i."\t";
+// 		if(isset($authors[$i][$j])) {
+// 			$authors[$i][$j] = trim($authors[$i][$j]);
+// 			if($authors[$i][$j] !== "") {
+// 				$has = true;
+// 				print $authors[$i][$j];
+// 				print "\t";
+// 			}
+// 		}
+// 	}
+// 	if($has === true) {
+// 		print "\n";
+// 	}
+// }
+
+$xlsx = SimpleXLSXGen::fromArray($data);
+$xlsx->saveAs('res.xlsx');
+
